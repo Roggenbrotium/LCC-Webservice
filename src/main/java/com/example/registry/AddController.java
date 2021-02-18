332e703d969bd3a20b93e727b33480793d819c19
SiTa <simon.tazreiter@gmx.at>
2021-02-18 10:10:43 +0100

    Database connection test

--- src/main/java/com/example/registry/AddController.java
+++ src/main/java/com/example/registry/AddController.java
@@ -1,8 +1,9 @@
 package com.example.registry;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.BufferedReader;
@@ -12,6 +13,8 @@ import java.io.InputStreamReader;
 @RequestMapping(method = {RequestMethod.GET, RequestMethod.PUT})
 public class AddController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     private final String uuid = uuid_gen();
     private final UserData data = new UserData(uuid, "publickey", "instancetype", "version", true);
 
@@ -22,7 +25,17 @@ public class AddController {
 
     @PostMapping("/post")
     public ResponseEntity<String> post() {
-        return new ResponseEntity<String>("POST Response", HttpStatus.OK);
+        return new ResponseEntity<String>(test_sql(), HttpStatus.OK);
+    }
+
+    public String test_sql(){
+        String sql = "INSERT INTO data (uuid, publickey, instancetype, version, status) VALUES('testuuid', 'pkey', 'typeb', '12.2.5', false);";
+        int rows = jdbcTemplate.update(sql);
+
+        if(rows > 0){
+            return "succes";
+        }
+        return "fail";
     }
 
     public String uuid_gen(){
--- src/main/java/com/zetcode/model/Data.java
+++
@@ -1,5 +0,0 @@
-package com.zetcode.model;
-
-public class Data {
-
-}
